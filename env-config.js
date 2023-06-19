60e77273dc0089ca861cd38bc77a3bcf86cf3c69
Nitiphoom Yaipangkaew <nitiphoom@looksocial.asia>
2023-06-19 11:39:40 +0700

    first commit :v:

--- env-config.js
+++ env-config.js
@@ -6,5 +6,7 @@ dotenv.config({ path: `.env.${ENV_BRANCH ? ENV_BRANCH : "dev"}` });
 module.exports = {
   env: {
     TEST_ENV: process.env.TEST_ENV,
+    ACCESS_TOKEN_NAME: process.env.ACCESS_TOKEN_NAME,
+    REFRESH_TOKEN_NAME: process.env.REFRESH_TOKEN_NAME,
   },
 };
